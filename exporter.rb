7ee800579d45d018d3dca1a4cbfa9cf7f7e72891
Arshi Aggarwal <arshiagg@users.noreply.github.com>
2017-07-28 11:24:17 -0700

    Debugged logic in exporter
    Signed-off-by: Arshi Aggarwal <arshiagg@users.noreply.github.com>

--- exporter.rb
+++ exporter.rb
@@ -2,19 +2,23 @@ require "httparty"
 require "json"
 require "sinatra"
 
+STDOUT.sync = true
+
 OVS_DB_PORT = 6640
+REQUIRED_KEYS = %w[external_ids name statistics]
 
 set :bind, '0.0.0.0'
 
 get '/metrics' do
-
   # get etcd IP
   etcd = ENV.fetch("CONTIV_ETCD").split("//").last
 
   #get netmaster IP
+  puts "fetching leading address"
   netmaster = JSON.parse(HTTParty.get("http://#{etcd}/v2/keys/contiv.io/lock/netmaster/leader").body)["node"]["value"]
 
   # Get a list of networks
+  puts "fetching networks"
   raw_networks = JSON.parse(HTTParty.get("http://#{netmaster}/api/v1/networks/").body)
 
   networks = []
@@ -29,6 +33,7 @@ get '/metrics' do
 
   # Get endpoints and endpoint info for each network
   networks.each do |net|
+    puts "fetching #{net} network data"
     raw_epstats = JSON.parse(HTTParty.get("http://#{netmaster}/api/v1/inspect/networks/#{net}/").body)
 
     tenant = raw_epstats["Config"]["tenantName"]
@@ -51,21 +56,49 @@ get '/metrics' do
     end
   end
 
+  puts "epInfo:"
+  puts epInfo.inspect
+  
+
   # get ovs stats
-  ovs_output = `ovs-vsctl --db=tcp:127.0.0.1:#{OVS_DB_PORT} list interface | egrep "name|external_ids|statistics"`
+  cmd = "ovs-vsctl --db=tcp:127.0.0.1:#{OVS_DB_PORT} list interface | egrep '^name|external_ids|statistics'"
+  puts "Running: #{cmd}"
+
+  ovs_output = `#{cmd}`
+
+  raise "failed to run ovs-vsctl (expected: 0, received: #{$?.exitstatus})" unless $?.exitstatus == 0
+
+  puts "Output:"
+  puts ovs_output
 
   # group ovs output by interface
-  interfaces = ovs_output.strip.split("\n").each_slice(4)
+  interfaces = ovs_output.strip.split("\n").each_slice(3).inject([]) do |memo, obj|
+    h = {}
+
+    obj.each do |item|
+      parts = item.split(":").map(&:strip)
+      h[parts.first] = parts.last
+    end
+
+    missing_keys = REQUIRED_KEYS - h.keys
+
+    raise "following keys are missing from the hash: #{missing_keys.join(", ")}" unless missing_keys.empty?
+    memo << h
+    memo
+  end
+  
+  puts "Interfaces:"
+  puts interfaces.inspect
 
   # parses data from OVS output and matches endpointIDs with epInfo map created above
-  interfaces.each do |ex_id, name, stats, status|
+  interfaces.each do |interface|
     epInfo.keys.each do |key|
-      if ex_id.include?(key)
+      if interface["external_ids"].include?(key)
         # get container interface
-        epInfo[key]["interface"] = name.split(":").last.gsub("\"", "").strip
+        epInfo[key]["interface"] = interface["name"].split(":").last.gsub("\"", "").strip
 
         # get stats into hash
-        epstats = stats.split(":").last.scan(/(\w+)=(\d+)/).to_h
+        epstats = interface["statistics"].split(":").last.scan(/(\w+)=(\d+)/).to_h
 
         #create key-value pairs and store into array
         info = "{" + epInfo[key].map{|k,v| "#{k}=\"#{v}\""}.join(", ") + "}"
